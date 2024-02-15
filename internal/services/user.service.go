213266e2abf7c6a7c8f4b7f36070f8823321b845
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2024-02-15 19:49:34 +0100

    fix: jwt token problem fixed

--- internal/services/user.service.go
+++ internal/services/user.service.go
@@ -27,27 +27,27 @@ func (s *Service) Register(user m.RegisterBody) (token string,err error) {
 		return  "", err
 	}
 
-	token = jwt.Create(id, user.Name, user.Email)
+	token = jwt.Create(id,user.Email, user.Name )
 	return token, nil
 }
 
 func (s *Service) Login(user m.LoginBody) (name string ,token string,err error) {
 	myUser, err := s.repo.Users.GetUserByEmail(user.Email)
 	if err != nil {
-		return "", "", errors.New("User not found")
+		return "", "", errors.New("user not found")
 	}
 	if err := bc.CheckPasswordHash(user.Password, myUser.Password); err != nil {
-		return "", "", errors.New("Invalid password")
+		return "", "", errors.New("invalid password")
 	}
 	
-	token = jwt.Create(myUser.ID.Hex(), myUser.Name, myUser.Email)
+	token = jwt.Create(myUser.ID.Hex(),myUser.Email, myUser.Name )
 	return myUser.Name,token, nil
 }
 
 func (s *Service) Getme(userId string) (user m.RegisterResponse , err error) {
 	user, err = s.repo.Users.GetOneByID(userId)
 	if err != nil {
-		return  m.RegisterResponse{},  errors.New("User not found")
+		return  m.RegisterResponse{},  errors.New("user not found")
 	}
 	
 	return  user, nil
